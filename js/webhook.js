82a7af9389bf2ad5b5b6492442d5a1006ac569ee
anjo0803 <68995159+anjo0803@users.noreply.github.com>
2020-10-06 14:36:28 +0200

    Add files via upload

--- js/webhook.js
+++ js/webhook.js
@@ -5,8 +5,10 @@
 const HOOK = {
     payload: {},
     send: function() {
-        if(localStorage.getItem('hook') == null || localStorage.getItem('token') == null || localStorage.getItem('auth') == null) console.error('No credentials provided, request denied');
-        else {
+        if(localStorage.getItem('hook') == null || localStorage.getItem('token') == null || localStorage.getItem('auth') == null) {
+            console.error('Necessary credentials not provided, request denied');
+            window.alert('No or not all necessary credentials are registered in your browser. Please get yourself registered by OWL Senior Staff first.');
+        } else {
             payload.username = localStorage.getItem('auth')
             $.ajax({
                 url: 'https://discordapp.com/api/webhooks/' + localStorage.getItem('hook') + '/' + localStorage.getItem('token'),
