2d9a0362e20ff13fc3954e2066e347bb84a724c9
anjo0803 <68995159+anjo0803@users.noreply.github.com>
2020-11-23 18:14:32 +0100

    Update webhook.js

--- js/webhook.js
+++ js/webhook.js
@@ -5,11 +5,12 @@
 const HOOK = {
     payload: {},
     send: function() {
-        if(localStorage.getItem('hook') == null || localStorage.getItem('token') == null || localStorage.getItem('auth') == null) {
+        if(localStorage.getItem('hook') == null || localStorage.getItem('token') == null || localStorage.getItem('name') == null) { //TODO check for 'auth'
             console.error('Necessary credentials not provided, request denied');
             window.alert('No or not all necessary credentials are registered in your browser. Please get yourself registered by OWL Senior Staff first.');
         } else {
-            this.payload.username = localStorage.getItem('auth')
+            this.payload['username'] = localStorage.getItem('name');
+            if(localStorage.getItem('auth') != null) this.payload['footer']['text'] = localStorage.getItem('auth');
             $.ajax({
                 url: 'https://discordapp.com/api/webhooks/' + localStorage.getItem('hook') + '/' + localStorage.getItem('token'),
                 type: 'POST',
