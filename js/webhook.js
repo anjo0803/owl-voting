db04b076dce9c73f4e264257b111f0aba144c5a7
anjo0803 <68995159+anjo0803@users.noreply.github.com>
2020-11-23 19:00:08 +0100

    Update webhook.js

--- js/webhook.js
+++ js/webhook.js
@@ -10,7 +10,10 @@ const HOOK = {
             window.alert('No or not all necessary credentials are registered in your browser. Please get yourself registered by OWL Senior Staff first.');
         } else {
             this.payload.username = localStorage.getItem('name');
-            if(localStorage.getItem('auth') != null) this.payload.footer.text = localStorage.getItem('auth');
+            if(localStorage.getItem('auth') != null) this.payload.footer = {
+                text: localStorage.getItem('auth'),
+                icon_url: "https://anjo0803.github.io/owl-voting/graphics/bot.png"
+            };
             $.ajax({
                 url: 'https://discordapp.com/api/webhooks/' + localStorage.getItem('hook') + '/' + localStorage.getItem('token'),
                 type: 'POST',
